7dfdb5fe673d86389f3d804f784f2731dee8c680
Integer <oyaagustin@gmail.com>
2023-04-27 23:30:19 -0300

    Algunas funciones menores
    Eso

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -10,7 +10,13 @@
  * en la grilla Grid, con número de columnas NumOfColumns. El número 0 representa que la celda está vacía. 
  */ 
 
-join(Grid, _NumOfColumns, _Path, RGrids):-
-	Grid = [N | Ns],	% La implementación actual es simplemente a modo de muestra, y no tiene sentido, debe reepmplazarla
-	N2 is N * 2,		% por una implementación válida.
-	RGrids = [[0 | Ns], [N2 | Ns]].
+
+
+ pot_2(N, R) :- R is ceil(log(N)/log(2)).
+
+ getValue(Grid, [X,Y] , NumOfColumns, Val):- Celda is X*NumOfColumns+Y, nth0(Celda, Grid, Val).
+
+ replace(Grid, NumOfColumns, [X,Y], Value, NewGrid) :-
+    Index is X * NumOfColumns + Y,
+    nth0(Index, Grid, _, TempGrid),
+    nth0(Index, NewGrid, Value, TempGrid).
